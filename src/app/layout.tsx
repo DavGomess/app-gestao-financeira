ed3ea9d702503d7d6f1daba35863e6c5ea0117be
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-08-09 14:30:12 -0300

    feat(layout): adicionar layout base, página inicial e assets

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Poppins, Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { SelectedProvider } from "@/contexts/SelectedContext";
+
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],   
+  variable: "--font-poppins",
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "meuSaldo | Controle suas finanças de forma simples",
+  description: "Controle suas finanças pessoais de forma prática com o MeuSaldo.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} ${geistMono.variable}`}>
+        <SelectedProvider>
+        {children}
+        </SelectedProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import Header from "./components/Header";
+import MenuLateral from "./components/MenuLateral";
+import Dashboard from "./dashboard/dashboard";
+import Transacoes from "./transacoes/transacoes";
+import Relatorios from "./relatorios/relatorios";
+import Metas from "./metas/metas";
+import Categorias from "./categorias/categorias";
+import Orcamento from "./orcamento/orcamento";
+import ContasPagar from "./contasPagar/contasPagar";
+import Configuracoes from "./configuracoes/configuracoes";
+import { useSelected } from "@/contexts/SelectedContext";
+
+export default function Home() {
+
+  const { selected } = useSelected();
+
+  return (
+    <div className="main">
+          <MenuLateral />
+        <div className="container p-0">
+          <Header/>       
+          {selected === "dashboard" && <Dashboard />}
+          {selected === "transacoes" && <Transacoes />}
+          {selected === "relatorios" && <Relatorios />}
+          {selected === "metas" && <Metas />}
+          {selected === "categorias" && <Categorias />}
+          {selected === "orcamento" && <Orcamento />}
+          {selected === "contasPagar" && <ContasPagar />}
+          {selected === "configuracoes" && <Configuracoes />}
+        </div>
+    </div>
+  );
+}
+
