1afc84b43a73d958b97b2ee43b15d8cb97f0cb73
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-09-09 21:14:20 -0300

    feat(server): adiciona rota /transacoes e ajusta importação

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import contasPagarRoutes from "./routes/contasPagarRoutes";
+import contasPagarRoutes from "./routes/contasPagarRoutes.ts";
 
 const app = express();
 
@@ -8,5 +8,6 @@ app.use(cors())
 app.use(express.json());
 
 app.use("/contasPagar", contasPagarRoutes);
+app.use("/transacoes", contasPagarRoutes);
 
 app.listen(4000, () => console.log("Servidor rodando na porta 4000"));
