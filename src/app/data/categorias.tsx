e7c8450e79ef42f3f9439a0df7acb528c7821fbd
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-09-11 20:18:06 -0300

    feat(data): adiciona arquivo categorias com lista de Receita e Despesa

---
+++ src/app/data/categorias.tsx
@@ -0,0 +1,5 @@
+export const categorias = {
+        Receita: ["Freelance", "Investimentos", "Salário", "Vendas", "Todos"], 
+        Despesa: ["Alimentação", "Educação", "Compras", "Entretenimento", "Saúde", "Transporte", "Moradia", "Carro", "Todos",]
+}
+
