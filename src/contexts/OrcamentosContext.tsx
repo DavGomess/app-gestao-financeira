7ac210f3ed5a00822f74ffa356a51cd4a25a17d3
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-09-21 22:28:01 -0300

    feat(context): adiciona OrcamentosContext com persistência em localStorage

---
+++ src/contexts/OrcamentosContext.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { Orcamento } from "@/types/CriarContaInput";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+
+interface  OrcamentosContextType {
+    orcamentos: Orcamento[];
+    adicionarOrcamento: (categoria: string, valor: number) => void;
+    removerOrcamento: (id: number) => void;
+    editarOrcamento: (id: number,categoria: string, valor: number) => void
+}
+
+const OrcamentosContext = createContext<OrcamentosContextType | undefined>(undefined);
+
+export function OrcamentosProvider({ children }: { children: ReactNode }) {
+    const [orcamentos, setOrcamentos] = useState<Orcamento[]>([]);
+
+    useEffect(() => {
+        const saved = localStorage.getItem("orcamentos");
+        if (saved) {
+            try {
+                setOrcamentos(JSON.parse(saved));
+            } catch (error) {
+                console.error("Error ao carregar orçamentos", error);
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("orcamentos", JSON.stringify(orcamentos));
+    }, [orcamentos]);
+
+    const adicionarOrcamento = (categoria: string, valor: number) => {
+        const novo: Orcamento = {
+            id: Date.now(),
+            categoria,
+            valor
+        }
+        setOrcamentos((prev) => [...prev, novo]);
+    };
+
+    const removerOrcamento = (id: number) => {
+        setOrcamentos((prev) => prev.filter((o) => o.id !== id));
+    };
+
+    const editarOrcamento = (id: number,categoria: string, valor: number) => {
+        setOrcamentos((prev) => prev.map((o) => o.id === id ? { ...o, categoria, valor } : o))
+    };
+
+
+
+    return (
+        <OrcamentosContext.Provider value={{ orcamentos, adicionarOrcamento, removerOrcamento, editarOrcamento }}>
+            {children}
+        </OrcamentosContext.Provider>
+    );
+}     
+
+export function useOrcamentos() {
+    const context = useContext(OrcamentosContext);
+    if (!context) {
+        throw new Error("useOrcamentos deve ser usado dentro de OrcamentosProvider");
+    }
+    return context; 
+}
