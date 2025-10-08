86101c8e879ad69feaa4fb560ae6bd3f3351eba8
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-10-07 22:41:53 -0300

    feat(menuLateral): permitir recolher/expandir menu ao clicar no botão

--- src/app/components/MenuLateral.tsx
+++ src/app/components/MenuLateral.tsx
@@ -2,30 +2,33 @@
 import Image from "next/image";
 import { menuItems } from "../data/menuItems";
 import { useSelected } from "@/contexts/SelectedContext";
+import { useState } from "react";
 
 
-export default function MenuLateral() {
-
+export default function MenuLateral( ) {
     const { selected, setSelected } = useSelected();
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    const recolherMenu = () => {
+        setIsCollapsed((prev) => !prev);
+    }
 
     return (
-        <div className="menuLateral">
+        <div className={`menuLateral ${isCollapsed ? "collapsed" : ""}`}>
                 <div className="logo ">
-                    <Image
+                    {!isCollapsed && <Image
                         src="/image/logo.png"
                         alt="logo"
                         width={50}
                         height={50}>
-                    </Image>
-                    <p className="p-0 m-0">meuSaldo</p>
+                    </Image>}
+                    {!isCollapsed &&  <p className="p-0 m-0">meuSaldo</p>}
                     <div className="menuIcon">
-                        <i className="bi bi-chevron-left menuIcon-strong"></i>
+                        <i className={`bi ${isCollapsed ? "bi-chevron-right menuIconStrongRight" : "bi-chevron-left menuIconStrongleft"} `} onClick={recolherMenu}></i>
                     </div>
                 </div>
-            {/* <hr /> */}
             <div className="opcoesMenuLateral">
                 <ul className="p-0 m-0">
-
                     {menuItems.map((item) => (
                         <div key={item.id}>
                             <li
@@ -40,7 +43,7 @@ export default function MenuLateral() {
                             >
                                 <div className="link">
                                     <i className={`bi ${item.icon}`}></i>
-                                    <span>{item.label}</span>
+                                    {!isCollapsed && <span>{item.label}</span>}
                                 </div>
                             </li>
                             {item.id === "contasPagar" && <hr />}
