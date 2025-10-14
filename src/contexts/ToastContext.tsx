1bd1ee4ae51efbddf861d8682f3cadded3052294
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-10-13 22:40:21 -0300

    feat(toastContext): criar contexto para exibição de toasts com suporte a múltiplas notificações

---
+++ src/contexts/ToastContext.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import ToastMessage from "@/app/components/ToastMessage";
+import { ToastProps } from "@/types/CriarContaInput";
+import { createContext, ReactNode, useContext, useState } from "react";
+
+
+interface ToastContextType {
+    showToast: (message: string, type?: ToastProps["type"]) => void;
+}
+
+const ToastContext = createContext<ToastContextType | undefined>(undefined);
+
+export function ToastProvider({ children }: { children: ReactNode }) {
+    const [toasts, setToasts] = useState<ToastProps[]>([]);
+
+    const showToast = (message: string, type: ToastProps["type"] = "primary") => {
+        const id = Date.now().toString();
+        setToasts((prev) => [...prev, { id, message, type }]);
+
+        setTimeout(() => {
+            setToasts((prev) => prev.filter((t) => t.id !== id));
+        }, 3500);
+    };
+
+    return (
+        <ToastContext.Provider value={{ showToast }}>
+            {children}
+            {toasts.map((toast) => (
+                <ToastMessage
+                    key={toast.id}
+                    id={toast.id}
+                    message={toast.message}
+                    type={toast.type}
+                />
+            ))}
+        </ToastContext.Provider>
+    )
+}
+
+export function useToast() {
+    const context = useContext(ToastContext);
+    if (!context) throw new Error("useToast deve ser usado dentro de ToastProvider");
+    return context;
+}
