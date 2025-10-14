a589016c0c51a29c4b9f03863635ede26a5f01e1
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-10-13 22:38:16 -0300

    feat(themeContext): implementar alternância entre temas claro e escuro

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface ThemeContextType {
+    theme: "light" | "dark";
+    toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+    const [theme, setTheme] = useState<"light" | "dark">("light");
+
+    useEffect(() => {
+        const savedTheme = localStorage.getItem("theme") as "light" | "dark" | null;
+        if (savedTheme) {
+            setTheme(savedTheme);
+            document.body.classList.toggle("dark-theme", savedTheme === "dark");
+        }
+    }, []);
+
+    const toggleTheme = () => {
+        const newTheme = theme === "light" ? "dark" : "light";
+        setTheme(newTheme);
+        document.body.classList.toggle("dark-theme", newTheme === "dark");
+        localStorage.setItem("theme", newTheme);
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+}
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+    if (!context) throw new Error("useTheme deve ser usado dentro de ThemeProvider")
+    return context;
+}
