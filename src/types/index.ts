dae08359a001425cdfea1a30d24be05b47217aaa
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-11-02 19:05:35 -0300

    feat(types): centraliza interfaces de domínio (transacoes, categorias, orcamentos, metas, auth, redefinirSenha)

---
+++ src/types/index.ts
@@ -0,0 +1,123 @@
+import { Prisma } from "@prisma/client";
+import { StatusConta } from "../utils/status";
+
+export interface CriarContaInput {
+    nome: string;
+    valor: number;
+    categoriaId?: number;
+    data: string;
+    status: StatusConta;
+}
+
+export interface ContaLocal {
+    id: number; 
+    nome: string;
+    valor: number;
+    categoria: string;
+    categoriaId: number | null;
+    data: string;
+    status: StatusConta;
+    statusAnterior?: StatusConta;
+}
+
+
+export interface ContaFromAPI {
+    id: number;
+    nome: string;
+    valor: number;
+    data: string;
+    status: string;
+    userId: number;
+    categoriaId: number | null;
+    statusAnterior?: string;
+    categoria: {
+    id: number;
+    nome: string;
+    tipo: string;
+    } | null;
+}
+
+export interface Transacao {
+    id: number;
+    contaId: number;
+    nome: string;
+    valor: number;
+    categoriaId: number;
+    data: string;
+    tipo: "receita" | "despesa"; 
+    status: string;
+}
+
+
+export type TransacaoFromAPI = Prisma.TransacaoGetPayload<{
+    include: {
+        categoria: true;
+        conta: true;
+    };
+}>;
+
+export type TransacaoTipo = "receita" | "despesa";
+export type TransacaoStatus = "pendente" | "paga" | "vencida";
+
+
+export interface TransacaoLocal {
+    id: number;
+    valor: number;
+    tipo: TransacaoTipo;
+    data: string;
+    status: TransacaoStatus;
+    categoria: string;
+    categoriaId: number | null;
+    contaNome: string;
+    contaId: number;
+    nome: string;
+}
+
+export type CategoriaFromAPI = Prisma.CategoriaGetPayload<true>;
+
+export interface CategoriaLocal {
+    id:number;
+    nome: string;
+    tipo: "receita" | "despesa";
+}
+
+export interface PeriodoSelecionado  {
+    tipo: "predefinido" | "personalizado";
+    dias?: number; 
+    inicio?: Date;
+    fim?: Date;
+};
+
+export interface ToastProps {
+    id: string;
+    message: string;
+    type?: "primary" | "success" | "danger" | "warning" | undefined;
+};
+
+export type OrcamentoFromAPI = Prisma.OrcamentoGetPayload<{ include: { categoria: true } }>;
+
+export interface OrcamentoLocal {
+    id: number;
+    categoriaId: number;
+    valor: number;
+}
+
+export type OrcamentoInput = Omit<OrcamentoLocal, "id">;
+
+export type MetaFromAPI = Prisma.MetaGetPayload<true>;
+
+export interface MetaLocal {
+    id: number;
+    titulo: string;
+    categoriaId: number | null;
+    valor: number;  
+    valorAtual: number;
+    prazo: string;   
+}
+
+export type MetaInput = Omit<MetaLocal, "id" | "valorAtual">;
+
+
+export interface JwtPayload {
+    userId: number;
+}
