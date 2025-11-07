80e67df811088fb813bd6712dfbf3052fc502924
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-11-07 15:25:54 -0300

    feat(setupTest): adiciona setup global com servidor isolado e cleanup completo

---
+++ src/setupTests.ts
@@ -0,0 +1,26 @@
+import { prisma } from "@/lib/prisma";
+import { Server } from "http";
+import app from "./app";
+
+let server: Server  ;
+
+beforeAll(async () => {
+  const port = 4001 + Math.floor(Math.random() * 1000);
+    server = app.listen(port);
+    console.log(`Test server rodando na porta ${port}`);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+});
+
+afterEach(async () => {
+
+    await prisma.orcamento.deleteMany();
+    await prisma.transacao.deleteMany();
+    await prisma.categoria.deleteMany();
+    await prisma.contasPagar.deleteMany();
+    await prisma.user.deleteMany();
+});
+
+afterAll(async () => {
+    await prisma.$disconnect();
+    server.close();
+});
