62bde66d045714a61e7ae75ee03fafd0220ca34f
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-11-14 22:00:59 -0300

    fix(Login): adiciona setTimeout(0) no if(user) do login - resolve erro Router update during render

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,7 +18,7 @@ export default function Login() {
 
     if (authLoading) return null;
     if (user) {
-        router.replace("/"); 
+        setTimeout(() => router.replace("/"), 0); 
         return null;
     }
 
